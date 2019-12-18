12a19cbecdc6e27bacd1efe7404b2bbb39175abf
Jose Martinez <jmmrcp@gmail.com>
2019-12-18 12:48:09 +0100

    Creating archives

---
+++ devenv-installer.ps1
@@ -0,0 +1,4 @@
+# Install Chocolatey and applications
+# Running this runs the above
+
+iex ((new-object net.webClient).DownloadString('https://gist.githubusercontent.com/amogram/8217460/raw/ecdc2b5050af2b4803488c86e1ea07e93164de6b/chocolatey-env-setup.ps1'))
