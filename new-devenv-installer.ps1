ce33c0f057cf81b00684b19dce79595480e65d49
Jose Martinez Ruiz <jmmrcp@gmail.com>
2020-07-28 22:09:48 +0200

    Add files via upload
    new

---
+++ new-devenv-installer.ps1
@@ -0,0 +1,24 @@
+# Install Chocolatey and applications
+# Running this runs the above
+
+$here = Split-Path -parent $MyInvocation.MyCommand.Definition
+$script = $MyInvocation.MyCommand.Name
+
+$identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
+if (-not $principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Write-Warning "Not running with administrative rights. Attempting to elevate..."
+    $command = "-ExecutionPolicy bypass -File `"$here\$script`""
+    Start-Process powershell -verb runas -argumentlist $command
+    Exit
+}
+
+# Attempt to upgrade chocolatey (and all installed packages) else (if the command fails) install it.
+try {
+    choco upgrade all -y -r --no-progress --log-file=$LogFile
+    } catch {
+    # [System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+
+iex ((new-object net.webClient).DownloadString('https://raw.githubusercontent.com/jmmrcp/choco/master/chocolatey-env-setup.ps1'))
